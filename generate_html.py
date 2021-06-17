76cee94faf78793bd158a5788ef97b95a9a4a33b
Trevor Phillips <trevphil3@gmail.com>
2021-06-17 17:43:45 +0200

    Update results, plots, and scripts

--- generate_html.py
+++ generate_html.py
@@ -4,6 +4,10 @@ import numpy as np
 import pandas as pd
 import webbrowser
 
+N_NOISE_TRIALS = 50
+INLIER_ERROR = 0.01
+MIN_ESTIMATES_FOR_FAILURE = 0.01
+
 def get_color_name(i):
   colors = [
     'AliceBlue',
@@ -20,11 +24,11 @@ def image_path_to_link(p):
 
 def info_table():
   keys = {
-    'Failure': '1 if stereo failed OR if when projecting all estimated 3D points into the nearest (integer) pixel coordinate in the depth image, less than 10% of the image is "covered".',
+    'Failure': f'1 if stereo failed OR if when projecting all estimated 3D points into the nearest (integer) pixel coordinate in the depth image, less than {MIN_ESTIMATES_FOR_FAILURE * 100.0}% of the image is "covered".',
     'Debug depth': 'A link to a "debug image" comparing ground truth vs. estimated depth. Coloring is based on z-coordinate in camera frame, NOT ray length from camera to surface, and is scaled by the overall (min, max) z-coordinate from estimated and ground truth points.',
     'Debug stereo': 'A link to a "debug image" from the stereo algorithm, this is specific and different for each algorithm.',
     'Fraction estimated px': 'Calculated by projecting all estimated 3D points into the nearest (integer) pixel coordinate in the depth image, and calculating how many pixels are filled',
-    'Fraction inliers': 'The fraction of estimated points with an error below 6 cm. Based on estimated vs. true ray length from camera origin to surface.',
+    'Fraction inliers': f'The fraction of estimated points with relative error below {INLIER_ERROR * 100.0}%. Based on estimated vs. true ray length from camera origin to surface.',
     'Fraction outliers': '1 - (fraction of inliers)',
     'G.T. relative error': 'Defined as (estimated depth - true depth) / true depth. Based on estimated vs. true ray length from camera origin to surface.',
     'Max abs. error': 'Maximum error in meters of any point estimate. Based on estimated vs. true ray length from camera origin to surface.',
@@ -37,10 +41,25 @@ def info_table():
   df = pd.DataFrame(data, columns=['Description'], index=index)
   return df
 
+def info_section():
+  return f"""<h4>
+  Values reported in the tables show μ ± 1-σ for each metric, when averaged over all
+  frames (if the test is composed of more than 1 stereo image pair) and all trials.
+
+  If a test had artificial noise added (pose or image noise), {N_NOISE_TRIALS} Monte Carlo trials were run.
+  Otherwise for tests without noise, only 1 trial was run.
+
+  The <em>Timeseries</em> plots show each metric over time, with dotted lines indicating ± 3-σ.
+  The <em>Boxplot</em> plots show quartiles Q0, Q1, Q2, Q3, and Q4 for each metric, from all frames and trials.
+  </h4>"""
+
 def render_html(test_sets):
   html = "<!DOCTYPE html><html><head></head><body>"
+  html += "<h4>The following metrics are computed on a <em>per-frame</em> basis (i.e. each time stereo runs on an image pair):</h4>"
   info = info_table()
   html += info.to_html(escape=False)
+  html += "<hr/><br/>"
+  html += info_section()
 
   i = 0
   for test_set_name, df in test_sets.items():
--- main.py
+++ main.py
@@ -33,11 +33,26 @@ def get_files(basepath):
 
 
 def parse_summary_file(filepath):
-  stats = defaultdict(lambda: np.nan)
+  stats = defaultdict(lambda: defaultdict(lambda: np.nan))
+  if not filepath.exists():
+    return stats
+
   with open(filepath, 'r') as f:
     for line in f:
-      name, val = line.split(':')[:2]
-      stats[name.strip()] = float(val.strip())
+      parts = line.split(',')
+      if len(parts) == 9:
+        metric_name = parts[0].strip()
+        n, mean, std, q00, q25, q50, q75, q100 = [float(p.strip()) for p in parts[1:]]
+        stats[metric_name] = {
+          'n': int(n),
+          'mean': mean,
+          'std': std,
+          'q00': q00,
+          'q25': q25,
+          'q50': q50,
+          'q75': q75,
+          'q100': q100
+        }
   return stats
 
 #################################################################
@@ -59,7 +74,7 @@ else:
   shutil.copytree('/tmp/sfm_results', './sfm_results')
 
 for f in os.listdir('./plots'):
-  if f.endswith('.png'):
+  if f.endswith('.pdf') or f.endswith('.png') or f.endswith('.jpg'):
     os.remove(os.path.join('./plots', f))
 
 basepath = Path('./sfm_results')
@@ -126,22 +141,59 @@ def make_dataframe():
 #################################################################
 
 def parse_timeseries_file(filename):
-  df = pd.read_csv(filename)
-  df[df['Status'] != 'success'] = np.nan
-  return df
+  if not filename.exists():
+    return None
+  return pd.read_csv(filename, skipinitialspace=True)
 
-def generate_timeseries_plot(data, column, ymin=None, ymax=None, title=None):
+def latex_sanitize(s):
+  return s.replace('%', '\%').replace('<=', '$\le$').replace('>=', '$\ge$').replace('<', '$<$').replace('>', '$>$')
+
+colors = [plt.get_cmap('Set2')(1. * i / len(stereo_methods)) for i in range(len(stereo_methods))]
+method2color = dict(zip(stereo_methods, colors))
+
+def generate_timeseries_plot(data, column, title=None):
   fig, ax = plt.subplots(1, 1, figsize=(10, 5))
-  for method, df in data.items():
-    x = df['Frame ID']
-    y = df[column]
-    ax.plot(x, y, label=method, linewidth=0.7)
+  for method, (mean, std) in data.items():
+    x = mean['Frame ID']
+    y_center = mean[column]
+    c = method2color[method]
+    ax.plot(x, y_center, label=method, linewidth=1.0, c=c)
+    if False: # np.sum(std[column]) > 1e-5:
+      y_upper = y_center + (3 * std[column])
+      y_lower = y_center - (3 * std[column])
+      ax.plot(x, y_lower, linewidth=0.6, linestyle='dashed', c=c)
+      ax.plot(x, y_upper, linewidth=0.6, linestyle='dashed', c=c)
   ax.legend()
   ax.set_xlabel('Image sequence number')
-  ylabel = column.replace('%', '\%').replace('<=', '$\le$').replace('>=', '$\ge$').replace('<', '$<$').replace('>', '$>$')
-  ax.set_ylabel(ylabel)
-  if (ymin is not None) and (ymax is not None):
-    ax.set_ylim(ymin, ymax)
+  ax.set_ylabel(latex_sanitize(column))
+  if title is not None:
+    ax.set_title(title)
+  filename = './plots/%s.pdf' % str(uuid.uuid1())
+  plt.savefig(filename)
+  plt.close(fig=fig)
+  return Path(filename)
+
+def generate_boxplot(data, metric, title=None):
+  fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+  x, labels, clrs = [], [], []
+  for method, statistics in data.items():
+    quartiles = [
+      statistics[metric]['q00'],
+      statistics[metric]['q25'],
+      statistics[metric]['q50'],
+      statistics[metric]['q75'],
+      statistics[metric]['q100']
+    ]
+    x.append(quartiles)
+    label = f'{method}\n[N={statistics[metric]["n"]}]'
+    labels.append(label.replace(' (', '\n('))
+    clrs.append(method2color[method])
+  bplot = ax.boxplot(x, labels=labels, whis=[0, 100], patch_artist=True)
+  for patch, c in zip(bplot['boxes'], clrs):
+    patch.set_facecolor(c)
+  plt.setp(bplot['medians'], color='black')
+  ax.yaxis.grid(True)
+  ax.set_ylabel(latex_sanitize(metric))
   if title is not None:
     ax.set_title(title)
   filename = './plots/%s.pdf' % str(uuid.uuid1())
@@ -155,8 +207,9 @@ def generate_timeseries_plot(data, column, ymin=None, ymax=None, title=None):
 
 test_sets = defaultdict(lambda: make_dataframe())  # Test set name --> table of results (dataframe)
 timeseries = defaultdict(lambda: dict())  # (test set, motion) --> dict from (method name --> timeseries)
+all_statistics = defaultdict(lambda: dict())  # (test_set, motion) --> dict from (method name --> statistics)
 timeseries_cols = None
-blacklisted_ts_cols = ('Frame ID', 'Failure', 'Status')
+blacklisted_cols = ('Frame ID', 'Failure', 'Status')
 
 for test_dir in tqdm(test_dirs):
   debug_image_files = get_files(test_dir / 'debug')
@@ -172,31 +225,35 @@ for test_dir in tqdm(test_dirs):
   test_set_name = test_dir.parts[-2]
   stereo_method = test_dir.parts[-3]
 
+  if test_set_name == 'AirSim Rocks':
+    test_set_name = 'AirSim Rocks [No Noise]'
+
   df = test_sets[test_set_name]
 
-  if stats['Failure'] == 1:
-    df[stereo_method][motion]['Failure'] = 1
-  else:
-    for stat_name, stat_value in stats.items():
-      df[stereo_method][motion][stat_name] = stat_value
+  for stat_name, statistics in stats.items():
+    df[stereo_method][motion][stat_name] = f"{statistics['mean']} ± {statistics['std']}"
 
   if len(debug_image_files) == 1:
     df[stereo_method][motion]['Debug stereo'] = image_path_to_link(debug_image_files[0])
   if len(depth_image_files) == 1:
     df[stereo_method][motion]['Debug depth'] = image_path_to_link(depth_image_files[0])
 
-  ts = parse_timeseries_file(test_dir / 'stats' / 'timeseries.csv')
-  if len(ts) > 1:
-    timeseries[(test_set_name, motion)][stereo_method] = ts
-    ts_cols = set([c for c in ts.columns if c not in blacklisted_ts_cols])
+  ts_mean = parse_timeseries_file(test_dir / 'stats' / 'timeseries_mean.csv')
+  ts_std = parse_timeseries_file(test_dir / 'stats' / 'timeseries_std.csv')
+  if (ts_mean is not None) and (ts_std is not None) and \
+      (len(ts_mean) > 1) and (len(ts_mean) == len(ts_std)):
+    timeseries[(test_set_name, motion)][stereo_method] = (ts_mean, ts_std)
+    ts_cols = set([c for c in ts_mean.columns if c not in blacklisted_cols])
     if timeseries_cols is None:
       timeseries_cols = ts_cols
     else:
       timeseries_cols = timeseries_cols.intersection(ts_cols)
+  
+  all_statistics[(test_set_name, motion)][stereo_method] = stats
 
 # Generate timeseries plots and add to summary tables
-print('Generating plots...')
-timeseries_cols = list(sorted(timeseries_cols))
+print('Generating timeseries plots...')
+timeseries_cols = list([s.strip() for s in sorted(timeseries_cols)])
 for (test_set_name, motion), method2timeseries in timeseries.items():
   title = '%s: %s' % (test_set_name, motion)
   df = test_sets[test_set_name]
@@ -205,6 +262,18 @@ for (test_set_name, motion), method2timeseries in timeseries.items():
   for col in timeseries_cols:
     image_path = generate_timeseries_plot(method2timeseries, col, title=title)
     df['Timeseries'][motion][col] = image_path_to_link(image_path)
+  test_sets[test_set_name] = df
+
+# Generate box-whisker plots and add to summary tables
+print('Generating boxplots...')
+for (test_set_name, motion), stats_per_method in all_statistics.items():
+  title = '%s: %s' % (test_set_name, motion)
+  df = test_sets[test_set_name]
+  if 'Boxplot' not in df.columns:
+    df['Boxplot'] = '-'
+  for col in timeseries_cols:
+    image_path = generate_boxplot(stats_per_method, col, title=title)
+    df['Boxplot'][motion][col] = image_path_to_link(image_path)
   test_sets[test_set_name] = df
 
 # Remove (outer) rows in each dataframe which have no data
